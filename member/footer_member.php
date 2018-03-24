118ca82245caad2325b6e6adf39c7f677edfe01f
Muskaan Bakshi <13muskanbakshi@gmail.com>
2018-03-24 22:48:16 +1000

    register_process files to check email

---
+++ member/footer_member.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>footer</title>
+</head>
+
+<body>
+	<!--Footer-->
+<footer class="page-footer font-small blue pt-4 mt-4">
+
+    <!--Footer Links-->
+    <div class="container-fluid text-center text-md-left" style="background-color: black;colour:white;">
+        <div class="row">
+
+            <!--First column-->
+            <div class="col-md-6">
+                <h5 class="text-uppercase">Footer Content</h5>
+                <p>Here you can use rows and columns here to organize your footer content.</p>
+            </div>
+            <!--/.First column-->
+
+            <!--Second column-->
+            <div class="col-md-6">
+                <h5 class="text-uppercase">Links</h5>
+                <ul class="list-unstyled">
+                    <li>
+                        <a href="#!">Link 1</a>
+                    </li>
+					</div
+					<br><br>
+					</div>
+			</div>
+			</div>
+			</div>
+	</footer>
+	</body>
+</html>
