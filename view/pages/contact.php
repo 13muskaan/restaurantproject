5bf2993d95639f68dd5e1751334428e8637cfb41
Muskaan Bakshi <13muskanbakshi@gmail.com>
2018-04-20 22:59:55 +1000

    included images (for logo), included contact info in contact page, updated draft design of home, updated header file

--- view/pages/contact.php
+++ view/pages/contact.php
@@ -6,35 +6,117 @@
 <head>
 	<title>MY BANYAN TREE | CONTACT</title>
 	<style>
-/*style map*/
-#map {
-	height: 350px;
-	width: 50%; /*fix this */
-}
-</style>
-<script> 
-	/*code for Google Maps*/
-	function initMap(){
-	var location = {lat: -27.456668, lng: 153.041047}; /*location, change location*/
-		var map = new google.maps.Map(document.getElementById("map"), {
-			zoom: 15, 
-			center: location
-		});
-		var marker = new google.maps.Marker({
-			position: location,
-				map: map
-		})
+		/*style map*/
+		
+		#map {
+			height: 350px;
+			width: 50%;
+			/*fix this */
 		}
-	</script> 
-  <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBctR_0ZgN-uxBHmJafdjcUdR35NuOS7O4&callback=initMap"></script>
+		/*style search bar*/
+		
+		#custom-search-input {
+			padding: 3px;
+			border: solid 1px #E4E4E4;
+			border-radius: 6px;
+			background-color: #fff;
+		}
+		
+		#custom-search-input input {
+			border: 0;
+			box-shadow: none;
+		}
+		
+		#custom-search-input button {
+			margin: 2px 0 0 0;
+			background: none;
+			box-shadow: none;
+			border: 0;
+			color: #666666;
+			padding: 0 8px 0 10px;
+			border-left: solid 1px #ccc;
+		}
+		
+		#custom-search-input button:hover {
+			border: 0;
+			box-shadow: none;
+			border-left: solid 1px #ccc;
+		}
+		
+		#custom-search-input .glyphicon-search {
+			font-size: 23px;
+		}
+	</style>
+	<script>
+		/*code for Google Maps*/
+		function initMap() {
+			var location = {
+				lat: -27.456668,
+				lng: 153.041047
+			}; /*location, change location*/
+			var map = new google.maps.Map( document.getElementById( "map" ), {
+				zoom: 15,
+				center: location
+			} );
+			var marker = new google.maps.Marker( {
+				position: location,
+				map: map
+			} )
+		}
+	</script>
+	<script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBctR_0ZgN-uxBHmJafdjcUdR35NuOS7O4&callback=initMap"></script>
 </head>
 
 <body>
 	<!--contact page-->
-	<h2> Locate us here.</h2>
-	<div id="map"></div>
-	
-<!-- end here-->
+
+	<div class="container">
+		<div class="containersearch" style="float: right;">
+			<div class="row">
+				<div class="col-md-6">
+					<div id="custom-search-input">
+						<div class="input-group col-md-12">
+							<input type="text" class="form-control input-lg" placeholder="Search directions..."/>
+							<span class="input-group-btn">
+                        <button class="btn btn-info btn-lg" type="submit">
+                            <i class="glyphicon glyphicon-search"></i>
+                        </button>
+                    </span>
+						
+
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="row-fluid">
+			<div class="span8">
+				<h2> Locate us here.</h2>
+				<div id="map"></div>
+			</div>
+
+			<div class="span4">
+				<h4>Our Contact details.</h4>
+				<address>
+    			<strong>My Banyan Tree</strong><br>
+    			4/9 Doggett Street<br>
+    			Fortitude Valley
+    			Brisbane
+    			4006<br>
+    			<abbr title="Phone">P:</abbr> 07 3358 4006<br>
+    			<abbr title="Phone">P:</abbr> 07 3358 4009<br>
+				<abbr title="Email">E: </abbr> bakshi.muskaan@gmail.com<br>
+   				<abbr title="OpeingTime"> <strong> Open Hours</strong></abbr> <br>
+   				Mon-Fri 	5pm-10pm 	<br>
+   				Sat-Sun 	5pm-10pm<br>
+    		</address>
+			
+
+
+			</div>
+		</div>
+	</div>
+	<!-- end here-->
 	<?php include('footer.php'); ?>
 </body>
 
--- view/pages/index.php
+++ view/pages/index.php
@@ -1,17 +1,32 @@
 <?php include('navigationbar.php');?>
-	<title>MY BANYAN TREE | HOME </title>
-	<!--- this is home/index page-->
-	<!-- Buttons for online food ordering companies-->
-	<div id="buttons" style="align-content: center;float: right;">
-	<h4> Order online. Enjoy meal at home.</h4>
-		<p> Or give us a call and we can deliveroo at your home.</p>
-	<a href="https://deliveroo.com.au/menu/brisbane/teneriffe/my-banyan-tree?day=today&time=ASAP" class="btn btn-primary btn-lg active" role="button" aria-pressed="true"style="background: url(../img/deliveroo.png)"><!--Deliveroo--></a>
-	<a href="https://www.menulog.com.au/restaurants-my-banyan-tree/menu" class="btn btn-primary btn-lg active" role="button" aria-pressed="true" style="background: url(../img/menulog.png)"><!--Menulog--></a>
-	<a href="https://www.eatnow.com.au/restaurant-7531/My-Banyan-Tree.html" class="btn btn-primary btn-lg active" role="button" aria-pressed="true" style="background: url(../img/eatnow.jpg)"><!--Eatnow--></a>
-	</div>
+<title>MY BANYAN TREE | HOME </title>
+<!--- this is home/index page-->
+<!-- Buttons for online food ordering companies-->
+<div id="body" style="width: 100%;">
 
-	<div class="container" style="width: 500px;">
-		<h2 style="text-align: center;">View our latest.</h2>
+	<div class="restaurantinfo">
+		<img src="../img/mybanyantreelogo.png" style="width: 500px; height: 300px;align-content: center;">
+		<p style="text-align: center;"> “In the centre of a small village in India, a Banyan Tree rises from the earth like a great mountain. Known as the Many Footed One, this remarkable tree has so many trunks it’s virtually a forest covering many acres. <br><br> A place for laughing and bartering, conversing and resting, romping and chasing meeting and imagining. The My Banyan Tree is not only in the heart of the village it is the heart of the village.”<br><br> The Banyan Tree, [bany-yuhn] gets its name from the Hindi word banian, meaning trader. The name appears to have been first bestowed popularly on a famous tree of this speciies growing near Gombroon, under which the Banians or Hindu traders settled at that part. <br><br> The mighty Banyan Tree is the national tree of India. It belongs to the mulberry family, Moraceae and is classified as Ficus bengalensis - a type of fig tree</p>
+		<br>
+		<h4> Our opening hours <br>
+	Mon-Fri 	5pm-10pm<br>
+	Sat-Sun 	5pm-10pm</h4>
+	</div>
+	<div id="buttons" style="align-content: center;float:center;">
+		<h2> Order online. Enjoy at home.</h2>
+		<p> Or give us a call and we can deliver at your home.</p>
+		<a href="https://deliveroo.com.au/menu/brisbane/teneriffe/my-banyan-tree?day=today&time=ASAP" class="btn btn-primary btn-lg active" role="button" aria-pressed="true" style="background: url(../img/deliveroo.png)">
+			<!--Deliveroo-->
+		</a>
+		<a href="https://www.menulog.com.au/restaurants-my-banyan-tree/menu" class="btn btn-primary btn-lg active" role="button" aria-pressed="true" style="background: url(../img/menulog.png)">
+			<!--Menulog-->
+		</a>
+		<a href="https://www.eatnow.com.au/restaurant-7531/My-Banyan-Tree.html" class="btn btn-primary btn-lg active" role="button" aria-pressed="true" style="background: url(../img/eatnow.jpg)">
+			<!--Eatnow-->
+		</a>
+	</div>
+	<div class="container" style="height: auto; width: 50">
+		<h2>View our latest. </h2>
 		<div id="myCarousel" class="carousel slide" data-ride="carousel">
 			<!-- Indicators -->
 			<ol class="carousel-indicators">
@@ -22,31 +37,17 @@
 
 			<!-- Wrapper for slides -->
 			<div class="carousel-inner">
-
-				<div class="item active" style="height: 500px">
-					<img src="../img/spices.jpg" alt="spices" style="width:100%;">
-					<div class="carousel-caption">
-						<h3>Los Angeles</h3>
-						<p>LA is always so much fun!</p>
-					</div>
+				<div class="item active">
+					<img src="../img/family.jpg" alt="Los Angeles" style="width:100%;">
 				</div>
 
 				<div class="item">
-					<img src="../img/family.jpg" alt="family" style="width:100%;">
-					<div class="carousel-caption">
-						<h3>Chicago</h3>
-						<p>Thank you, Chicago!</p>
-					</div>
+					<img src="../img/tiger.jpeg" alt="Chicago" style="width:100%;">
 				</div>
 
 				<div class="item">
-					<img src="../img/mybanyantree.jpg" alt="mybanyantree" style="width:100%;">
-					<div class="carousel-caption">
-						<h3>New York</h3>
-						<p>We love the Big Apple!</p>
-					</div>
+					<img src="../img/spices.jpg" alt="New york" style="width:100%;">
 				</div>
-
 			</div>
 
 			<!-- Left and right controls -->
@@ -55,16 +56,20 @@
       <span class="sr-only">Previous</span>
     </a>
 		
+
+
 			<a class="right carousel-control" href="#myCarousel" data-slide="next">
       <span class="glyphicon glyphicon-chevron-right"></span>
       <span class="sr-only">Next</span>
     </a>
 		
+
+
 		</div>
 	</div>
+</div>
 
-
-	<?php include( 'footer.php' );?>
+<?php include( 'footer.php' );?>
 
 </body>
 
